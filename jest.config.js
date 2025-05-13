b96a8831148d4d43da438ebc6897773e7bee3aaa
Oleg Yesterdays <olegyesterdays@gmail.com>
2025-05-13 22:53:06 +0300

    fix

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
-  // если вам нужны ESM-тесты на TypeScript, добавьте:
-  // extensionsToTreatAsEsm: ['.ts'],
-  // globals: { 'ts-jest': { useESM: true } },
-};
